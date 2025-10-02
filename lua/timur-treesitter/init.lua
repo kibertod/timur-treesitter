38a2d00459c9e00f475d2ed0fb41f14b7140dcbb
kibertod <cyber704d@gmail.com>
2025-10-02 15:36:04 +0300

    timur

--- lua/timur-treesitter/init.lua
+++ lua/timur-treesitter/init.lua
@@ -1,24 +1,10 @@
-local M = {}
-
-function M.setup(opts)
-	opts = opts or {}
-	config = vim.tbl_deep_extend("force", config, opts)
-
-	-- Register the parser
-	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-	parser_config.your_language = {
-		install_info = {
-			url = "https://github.com/kibertod/timur-treesitter",
-			files = { "parser/src/parser.c" },
-			branch = "main",
-			generate_requires_npm = true,
-			requires_generate_from_grammar = true,
-		},
-		filetype = "tmr",
-	}
-
-	-- Setup treesitter
-	require("nvim-treesitter.configs").setup(config)
-end
-
-return M
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.your_language = {
+	install_info = {
+		url = "https://github.com/kibertod/timur-treesitter",
+		files = { "parser/src/parser.c" },
+		generate_requires_npm = true,
+		requires_generate_from_grammar = true,
+	},
+	filetype = "tmr",
+}
