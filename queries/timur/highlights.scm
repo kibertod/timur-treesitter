31233eeb6b44bedef510b0c6ba7f05cbaf9614f3
kibertod <cyber704d@gmail.com>
2025-10-04 19:55:55 +0300

    timur

--- queries/timur/highlights.scm
+++ queries/timur/highlights.scm
@@ -3,18 +3,24 @@
 
 ; Keywords
 [
-    "return"
-    "class"
-    "if"
-    "elif"
-    "is"
-    "end"
-    "extends"
-    "var"
-    "then"
-    "loop"
-    "while"
+  "return"
+  "class"
+  "if"
+  "elif"
+  "is"
+  "end"
+  "extends"
+  "var"
+  "then"
+  "loop"
+  "while"
 ] @keyword
 
 ; Literals
 (literal string: (lit_str)) @string
+(literal integer: (lit_int)) @number
+(literal real: (lit_real)) @float
+[
+  "true"
+  "false"
+] @boolean
